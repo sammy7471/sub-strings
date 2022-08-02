635d7efa933e8e5e6164a807d824206ecbb691c0
Samuel Solomon <sammygideon01@gmail.com>
2022-08-02 20:25:59 +0000

    Split the array, Iterate through it and return the number of times it is found

--- sub_strings.rb
+++ sub_strings.rb
@@ -1,13 +1,20 @@
 def sub_strings(word, dictionary)
   word = word.downcase
+  split_word = word.split(" ")
   my_hash = {}
   output = my_hash
+
   if dictionary.include?(word)
     value = dictionary.count(word)
-    my_hash[word] = value
+    my_hash[word] = value 
+  else 
+    second_word = split_word.map do |x|
+        value = dictionary.count(x)
+        my_hash[x] = value
+    end  
   end  
   puts output
 end
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-sub_strings("below", dictionary)
+sub_strings("Howdy partner sit down! How's it going?", dictionary)
